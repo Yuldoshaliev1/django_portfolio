68440621b88c4a7b574b0f8424ae4acbd4d8c8a5
Yuldoshaliyev1 <akmalyuldoshaliev@gmail.com>
2022-12-25 22:53:07 +0500

    change base.html add about.html add footer.html change models.py change admin.py change views.py change models.py change admin.py change home.html change home.html change models.py change home.html change models.py change utils.py

--- apps/models.py
+++ apps/models.py
@@ -1,13 +1,9 @@
-import sys
-from io import BytesIO
-from PIL import Image
-from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db.models import Model, CharField, TextField, ImageField, DateField, ForeignKey, IntegerField, CASCADE, \
     DO_NOTHING, PROTECT
-from django.db.models.signals import post_delete
+
 from django.utils.text import slugify
 from tinymce.models import HTMLField
-from .signals import file_cleanup
+
 
 
 class AboutUser(Model):
@@ -72,36 +68,26 @@ class Project(Model):
     to_date = DateField()
     description = HTMLField()
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            while Project.objects.filter(slug=self.slug).exists():
+                slug = Project.objects.filter(slug=self.slug).first().slug
+                if '-' in slug:
+                    try:
+                        if slug.split('-')[-1] in self.title:
+                            self.slug += '-1'
+                        else:
+                            self.slug = '-'.join(slug.split('-')[:-1]) + '-' + str(int(slug.split('-')[-1]) + 1)
+                    except:
+                        self.slug = slug + '-1'
+                else:
+                    self.slug += '-1'
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title
 
-    def save(self, *args, **kwargs):
-        if self.title:
-            self.slug = slugify(self.title)
-
-
-        im = Image.open(self.image)
-
-        if im.mode in ("RGBA", "P"):
-            im = im.convert("RGB")
-
-        output = BytesIO()
-
-        # Resize/modify the image
-        im = im.resize((550, 370))
-
-        # after modifications, save it to the output
-        im.save(output, format='JPEG', quality=100)
-        output.seek(0)
-
-        # change the image field value to be the newly modified image value
-        self.image = InMemoryUploadedFile(output, 'ImageField', "%s.jpg" % self.image.name.split('.')[0], 'image/jpeg',
-                                          sys.getsizeof(output), None)
-
-        super(Project, self).save()
-
-
-post_delete.connect(file_cleanup, sender=Project)
 
 
 class Skill(Model):
@@ -113,46 +99,3 @@ class Skill(Model):
 
 
 
-
-#
-# class BlogCategory(Model):
-#     name = CharField(max_length=255)
-#
-#     class Meta:
-#         verbose_name = "Blog Category"
-#         verbose_name_plural = "Blog Categories"
-#
-#     def __str__(self):
-#         return self.name
-#
-#
-# def blog_directory_path(instance, filename):
-#     return 'blog/{0}/{1}'.format(strftime('%Y/%m/%d'), generate_file_name(25) + '.' + filename.split('.')[-1])
-#
-#
-# class Blog(Model):
-#     title = CharField(max_length=255)
-#     slug = CharField(max_length=255, unique=True)
-#     category = ForeignKey(BlogCategory, on_delete=CASCADE)
-#     description = HTMLField()
-#     image = ImageField(upload_to=blog_directory_path, null=True, blank=True)
-#     created_at = DateField(default=now)
-#
-#     def __str__(self):
-#         return self.title
-#
-#     def save(self, *args, **kwargs):
-#         if self.title:
-#             self.slug = slugify(self.title)
-#         super(Blog, self).save()
-#
-#     @property
-#     def photo(self):
-#         if self.image:
-#             return self.image.url
-#         else:
-#             return static("images/blog_default.jpg")
-#
-#     def get_absolute_url(self):
-#         return reverse_lazy('apps:blog-details', self.slug)
-#
--- apps/templatetags/chunks.py
+++ apps/templatetags/chunks.py
@@ -6,15 +6,15 @@ register = template.Library()
 
 
 @register.filter
-def chunks(value, chunk_length):
+def parts(value, part_length):
     """
     Breaks a list up into a list of lists of size <chunk_length>
     """
-    clean = int(chunk_length)
+    clean = int(part_length)
     i = iter(value)
     while True:
-        chunk = list(itertools.islice(i, clean))
-        if chunk:
-            yield chunk
+        part = list(itertools.islice(i, clean))
+        if part:
+            yield part
         else:
             break
